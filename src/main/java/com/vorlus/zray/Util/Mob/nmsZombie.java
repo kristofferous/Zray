d1a099ec45f5368b972f9a4ed6ada8a591be2fd3
Kristoffer <fraithor@gmail.com>
2021-08-22 18:10:30 +0200

    Initial commit

---
+++ src/main/java/com/vorlus/zray/Util/Mob/nmsZombie.java
@@ -0,0 +1,11 @@
+package com.vorlus.zray.Util.Mob;
+
+import net.minecraft.world.entity.EntityTypes;
+import net.minecraft.world.entity.monster.EntityZombie;
+import net.minecraft.world.level.World;
+
+public class nmsZombie extends EntityZombie {
+    public nmsZombie(World world) {
+        super(world);
+    }
+}
